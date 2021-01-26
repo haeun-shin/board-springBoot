b2977799b2f3c5e28abd6cc945a3f71bd8fdf793
HAEUN-SHIN <shinamon5@naver.com>
2021-01-26 21:26:14 +0900

    Add: 조회 목록 구현

--- src/main/java/com/jojodu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojodu/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,15 @@
 package com.jojodu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long>{
+
+    // springDataJpa에서 제공해주는 CRUD 외에도 추가로 쿼리를 작성을 해도 된다.
+    // 그럴때는 @Query로 사용해주면 된다.
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 
 }
--- src/main/java/com/jojodu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojodu/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.jojodu.book.springboot.service.posts;
 
 import com.jojodu.book.springboot.domain.posts.Posts;
 import com.jojodu.book.springboot.domain.posts.PostsRepository;
+import com.jojodu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojodu.book.springboot.web.dto.PostsResponseDto;
 import com.jojodu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojodu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 // @RequiredArgsConstructor: final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성
 @RequiredArgsConstructor
@@ -30,8 +33,18 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    // (readOnly=true) 이 옵션을 추가해주면 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선되기 때문에
+    // 등록, 수정, 삭제 기능이 없는 서비스 메소드에 사용하는 것이 좋다.
+    @Transactional(readOnly = true)
+    public Object findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
